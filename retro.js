a799b82ce7562ea8ecdd022eacaff6a5ee5eee8e
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2024-03-03 01:12:22 +0600

    setup all posts

--- retro.js
+++ retro.js
@@ -1,6 +1,6 @@
 
 let posts = async() => {
-    let res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts?category=coding')
+    let res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts?category=Comedy')
     let data = await res.json()
     let allData = data.posts
     allPosts(allData)
@@ -19,28 +19,37 @@ let allPosts = (allData) => {
                    <div class="bg-slate-400 p-4 rounded-[10px]">
                     <img src="Frame profail.png" alt="">
                    </div>
-                   <div><img class=" -translate-x-2 -translate-y-1" src="Status-red.png" alt="">
+                   <div>
+                   <img id="active" class=" -translate-x-2 -translate-y-1" src="Status-red.png" alt="">
+                   </div>
+
+                   <div id="unactive" class="hidden">
+                    <img class=" -translate-x-2 -translate-y-1" src="Status.png" alt="">
                    </div>
                 </div>
                 </div>
     
                    <div class="space-y-2">
                      <div class="flex gap-6">
-                        <p class="text-[#12132D99] font-semibold"># Coding</p>
-                        <p class="text-[#12132D99] font-semibold">Author : David Brown</p>
+                        <p class="text-[#12132D99] font-semibold"># ${postSetup.category}</p>
+                        <p class="text-[#12132D99] font-semibold">Author : ${postSetup.author.name}</p>
                      </div>
-                     <h4 class="text-[20px] font-semibold">JavaScript Programming: Advanced Techniques</h4>
-                    <p class="text-[#12132D99] pb-5 lg:w-[75%]">Take your JavaScript skills to the next level with advanced techniques and tips!</p>
+                     <h4 class="text-[20px] font-semibold">${postSetup.title}</h4>
+                    <p class="text-[#12132D99] pb-5 lg:w-[75%]">${postSetup.
+                        description
+                        }</p>
                    
                     <div class="flex justify-between items-center border-dashed border-t-2 border-gray-300 pt-5">
                         <div class="flex gap-2 lg:gap-8">
-                            <p class="flex gap-3 items-center text-[#12132D99]"> <img src="com.png" alt="">620</p>
-                        <p class="flex gap-3 items-center text-[#12132D99]"> <img src="tabler-icon-eye.png" alt=""> 2015</p>
-                        <p class="flex gap-3 items-center text-[#12132D99]"> <img src="tabler-icon-clock-hour-9.png" alt=""> 7min</p>
+                            <p class="flex gap-3 items-center text-[#12132D99]"> <img src="com.png" alt="">${postSetup.
+                                comment_count
+                                }</p>
+                        <p class="flex gap-3 items-center text-[#12132D99]"> <img src="tabler-icon-eye.png" alt=""> ${postSetup.view_count}</p>
+                        <p class="flex gap-3 items-center text-[#12132D99]"> <img src="tabler-icon-clock-hour-9.png" alt=""> ${postSetup.posted_time}min</p>
                         </div>
     
                          <div>
-                            <button>
+                            <button onclick="postBtn()">
                                 <img src="btn-img.png" alt="">
                             </button>
                          </div>
@@ -51,7 +60,22 @@ let allPosts = (allData) => {
         </div>
         `
         postContainer.appendChild(createPosts)
+
+       let unactive = document.getElementById('unactive')
+       let active = document.getElementById('active')
+       if(postSetup.isActive){
+        unactive.classList.remove('hidden')
+        active.classList.add('hidden')
+       }
+       else{
+        unactive.classList.add('hidden')
+        active.classList.remove('hidden')
+       }
+
     });
 }
 
+let postBtn = () => {
+    
+}
 posts()
